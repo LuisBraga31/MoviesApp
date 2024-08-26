55c999c4297a8cb5a481312860813e96ed595a85
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-08-26 17:35:00 -0300

    Criando estrutura

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#1e1e1e',
+      padding: 25,
+    },
+    headerText: {
+        marginTop: 30,
+        fontSize: 24,
+        lineHeight: 45,
+        color: '#FFF'
+    },
+    containerInput: {
+        
+    }
+  });
