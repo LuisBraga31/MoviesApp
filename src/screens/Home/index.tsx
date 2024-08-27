8866f5d655d637f80947748581d0e07dcb3691fd
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-08-27 15:19:22 -0300

    Adicioando CardMovies na pagina home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,55 @@
-import { Text, TextInput, View } from "react-native";
+import { FlatList, Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
 import { MagnifyingGlass } from "phosphor-react-native";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { CardMovie } from "../../components/CardMovie";
+
+interface Movie {
+    id: number;
+    title: string;
+    poster_path: string;
+    overview: string;
+}
 
 export function Home () {
+
+    const [discoveyMovies, setDiscoveyMovies] = useState<Movie[]>([]);
+
+    const loadingData = async () => {
+        const response = await api.get("/movie/popular")
+        setDiscoveyMovies(response.data.results)
+    }
+
+    useEffect(() => {
+        loadingData();
+    }, [])
+
     return (
         <View style={styles.container}> 
-            <Text style={styles.headerText}> O que você quer assistir ? </Text>
-            <View style={styles.containerInput}>
-                <TextInput style={styles.input} placeholderTextColor="#fff" placeholder="Buscar"/>
-                <MagnifyingGlass color="#fff" size={25} weight="light"/>
+            
+            <View style={styles.header}>
+                <Text style={styles.headerText}> O que você quer assistir ? </Text>
+                <View style={styles.containerInput}>
+                    <TextInput style={styles.input} placeholderTextColor="#fff" placeholder="Buscar"/>
+                    <MagnifyingGlass color="#fff" size={25} weight="light"/>
+                </View>
             </View>
+
+            <View>
+                <FlatList 
+                    data={discoveyMovies}
+                    numColumns={3}
+                    renderItem={(item) => (<CardMovie data={item.item}/>)}
+                    showsVerticalScrollIndicator={false}
+                    keyExtractor={(item) => item.id.toString()}
+                    contentContainerStyle={{
+                        padding: 35,
+                        paddingBottom: 100,
+                    }}
+                />
+            </View>
+
         </View>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,7 +4,10 @@ export const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#242A32',
+    },
+    header: {
       padding: 25,
+      paddingBottom: 5,
     },
     headerText: {
         marginTop: 30,
