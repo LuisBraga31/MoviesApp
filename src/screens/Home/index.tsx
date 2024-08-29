e10f236744cdca6424c2090f1e46d6fd72c94b65
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-08-29 10:26:40 -0300

    Configurando o search

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -15,8 +15,11 @@ interface Movie {
 export function Home () {
 
     const [discoveyMovies, setDiscoveyMovies] = useState<Movie[]>([]);
+    const [searchResultMoves, setSearchResultMovies] = useState<Movie[]>([]);
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(false);
+    const [noResult, setNoResult] = useState(false);
+    const [search, setSearch] = useState("");
 
     const loadingData = async () => {
         setLoading(true);
@@ -30,9 +33,35 @@ export function Home () {
         setLoading(false);
     }
 
+    const searchMovies = async (query: string) => {
+        setLoading(true);
+        const response = await api.get("/search/movie", {
+            params: {
+                query,
+            },
+        });
+        if(response.data.results.lenght ===  0) {
+            setNoResult(true);
+        } else {
+            setSearchResultMovies(response.data.results)
+        }
+        setLoading(false);
+    }
+
+    const handleSearch = (text: string) => {
+        setSearch(text)
+        if(text.length > 2) {
+            searchMovies(text);
+        } else {
+            setSearchResultMovies([]);
+        }
+    }
+
     useEffect(() => {
         loadingData();
     }, [])
+
+    const movieData = search.length > 2 ? searchResultMoves : discoveyMovies;
 
     return (
         <View style={styles.container}> 
@@ -40,14 +69,19 @@ export function Home () {
             <View style={styles.header}>
                 <Text style={styles.headerText}> O que você quer assistir ? </Text>
                 <View style={styles.containerInput}>
-                    <TextInput style={styles.input} placeholderTextColor="#fff" placeholder="Buscar"/>
+                    <TextInput style={styles.input} 
+                        placeholderTextColor="#fff" 
+                        placeholder="Buscar"
+                        value={search} 
+                        onChangeText={handleSearch}
+                    />
                     <MagnifyingGlass color="#fff" size={25} weight="light"/>
                 </View>
             </View>
 
             <View>
                 <FlatList 
-                    data={discoveyMovies}
+                    data={movieData}
                     numColumns={3}
                     renderItem={(item) => (<CardMovie data={item.item}/>)}
                     showsVerticalScrollIndicator={false}
