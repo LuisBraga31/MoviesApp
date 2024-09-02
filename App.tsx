b6aefaedcea403531715fa40ec6f64800cfe0f66
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-09-02 10:11:47 -0300

    Adicionando contexto no arquivo principal

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
 import { Routes } from './src/routes';
+import { MovieProvider } from './src/contexts/MovieContext';
 
 export default function App() {
   return (
     <>
-      <Routes/>
-      <StatusBar style="auto" />
+      <MovieProvider>
+        <Routes/>
+        <StatusBar style="auto" />
+      </MovieProvider>
     </>
   );
 }
