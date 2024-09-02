a024089155fcc36f56cd699d213ca64559fc1f73
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-09-02 10:12:07 -0300

    Configurando chave env

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     params: {
-        api_key: "ec305980c09d27f5d244c4f64ea6244b",
+        api_key: process.env.REACT_APIKEY,
         language: "pt-BR",
         include_adult: false,
     }
